e476edc90cb3c7013e30e8cf3fa440452022f4d6
hari-19 <hari19@outlook.in>
2020-05-26 20:01:07 +0530

    Updates

---
+++ precache-manifest.07f4ce03f3bbcd78706ebe592d97a707.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351f39330fd10bb5852351e2197ee28d",
+    "url": "/honestdesign-reactjs/index.html"
+  },
+  {
+    "revision": "9bd3e5c3463f1f73be01",
+    "url": "/honestdesign-reactjs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5664573c01e55885ab5b",
+    "url": "/honestdesign-reactjs/static/css/main.fe184cb2.chunk.css"
+  },
+  {
+    "revision": "9bd3e5c3463f1f73be01",
+    "url": "/honestdesign-reactjs/static/js/2.e0508f32.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/honestdesign-reactjs/static/js/2.e0508f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5664573c01e55885ab5b",
+    "url": "/honestdesign-reactjs/static/js/main.fc57da04.chunk.js"
+  },
+  {
+    "revision": "22c00f15f097141202b0",
+    "url": "/honestdesign-reactjs/static/js/runtime-main.dbc54506.js"
+  },
+  {
+    "revision": "13688fc6823cde8f3fda7de57a86b11b",
+    "url": "/honestdesign-reactjs/static/media/Graphic-Designing-bg.13688fc6.jpg"
+  }
+]);
